1fe5bda63ae5cb85eaafe62bbffbbe964757ba8d
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2023-05-12 14:20:59 -0300

    resolução ex escola

---
+++ escola/3n-min-max.py
@@ -0,0 +1,10 @@
+x = float(input('Digite um número: '))
+y = float(input('Digite outro número: '))
+z = float(input('Digite outro número: '))
+
+maior = max(x, y, z)
+menor = min(x,y,z)
+
+print('O maior número é: ', maior)
+print("O menor número é: ", menor)
+
